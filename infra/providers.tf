6e0f02966582895382711d2da32fc6823af50926
Abdisamad <30abdimoh16@gmail.com>
2026-09-16 11:30:12 +0100

    add remote Terraform state

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket       = "sammypulse-terraform-state"
+    key          = "sammypulse/terraform.tfstate"
+    region       = "eu-west-2"
+    use_lockfile = true
+  }
+}
+
 provider "aws" {
   region = "eu-west-2"
 }
